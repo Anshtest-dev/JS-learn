16d15fff7f2ba28ef17dc84ab82411e79372e15c
Anurag Singh <139899398+Anshtest-dev@users.noreply.github.com>
2025-05-16 23:09:00 +0000

    kam karo

---
+++ 01_basic/variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "ansh@gmail.com"
+var accountPassword = "1234"
+accountCity = "varanasi"
+let kamkafolder;
+
+//accountId =3 // not running
+
+console.log( 'The correct account is', accountId);
+
+console.table ([accountEmail, accountId, accountPassword, accountCity, kamkafolder])
